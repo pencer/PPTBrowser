eb898672c34f671866ba42bf963e90d8f1c009b5
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2016-02-07 22:02:37 +0900

    Recursive search for pptx files.

--- PPTBrowser/Form1.Designer.cs
+++ PPTBrowser/Form1.Designer.cs
@@ -33,6 +33,11 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -83,12 +88,66 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "label2";
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(510, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(290, 184);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 55);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "FindPPTX";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(357, 274);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 41);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Target Dir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(354, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(631, 210);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(168, 25);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Extract Slides";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(852, 467);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -111,6 +170,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- PPTBrowser/Form1.cs
+++ PPTBrowser/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using Microsoft.Office.Core;
 using Microsoft.Office.Interop.PowerPoint;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace PPTBrowser
 {
@@ -35,8 +36,13 @@ namespace PPTBrowser
 
         private void Form1_DragDrop(object sender, DragEventArgs e)
         {
-            string[] filename = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            label1.Text = filename[0];
+            string[] filenames = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string filename in filenames)
+            {
+                label1.Text = filename;
+                listBox1.Items.Add(filename);
+            }
+            //ExtractSlidesFromPPT(false);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -47,6 +53,11 @@ namespace PPTBrowser
 
         private void button1_Click(object sender, EventArgs e)
         {
+            ExtractSlidesFromPPT(true);
+        }
+
+        private void ExtractSlidesFromPPT(bool visible)
+        {
             Microsoft.Office.Interop.PowerPoint.Application app = null;
             Microsoft.Office.Interop.PowerPoint.Presentation ppt = null;
             try {
@@ -54,32 +65,33 @@ namespace PPTBrowser
                 app = new Microsoft.Office.Interop.PowerPoint.Application();
                 
                 // 表示する
-                app.Visible = Microsoft.Office.Core.MsoTriState.msoTrue;
-                //app.Visible = Microsoft.Office.Core.MsoTriState.msoFalse;
+                app.Visible = (visible) ? Microsoft.Office.Core.MsoTriState.msoTrue : Microsoft.Office.Core.MsoTriState.msoFalse;
 
-                String pptfilename = label1.Text;
-                // オープン
-                ppt = app.Presentations.Open(pptfilename,
-                    Microsoft.Office.Core.MsoTriState.msoTrue,
-                    Microsoft.Office.Core.MsoTriState.msoFalse,
-                    Microsoft.Office.Core.MsoTriState.msoTrue);
-
-//                app.Presentations[1].SaveAs(textBox1.Text + "\\b.png", PpSaveAsFileType.ppSaveAsPNG);
-                //ppt.SaveAs(textBox1.Text + "\\a.png", PpSaveAsFileType.ppSaveAsPNG);
-                // https://msdn.microsoft.com/JA-JP/library/office/ff746030.aspx
-                String basefilename = Properties.Settings.Default.BaseFolderPath + "\\" + pptfilename.Replace("\\","_").Replace(":","_");
-                label2.Text = "Slides.count=" + ppt.Slides.Count + ", " + basefilename;
-                for (int i = 1; i <= ppt.Slides.Count; i++)
+                foreach (string item in listBox1.Items)
                 {
-                    // スライド番号は１から始まるのに注意
-                    ppt.Slides.Range(i).Export(basefilename + i.ToString("_%03d") + ".png", "png", 640, 480);
+                    String pptfilename = item;// label1.Text;
+                    // オープン
+                    ppt = app.Presentations.Open(pptfilename,
+                        Microsoft.Office.Core.MsoTriState.msoTrue,
+                        Microsoft.Office.Core.MsoTriState.msoFalse,
+                        Microsoft.Office.Core.MsoTriState.msoFalse);
+
+                    // https://msdn.microsoft.com/JA-JP/library/office/ff746030.aspx
+                    String basefilename = Properties.Settings.Default.BaseFolderPath + "\\" + pptfilename.Replace("\\", "_").Replace(":", "_");
+                    label2.Text = "Slides.count=" + ppt.Slides.Count + ", " + basefilename;
+                    for (int i = 1; i <= ppt.Slides.Count; i++)
+                    {
+                        // スライド番号は１から始まるのに注意
+                        ppt.Slides.Range(i).Export(basefilename + i.ToString("_%03d") + ".png", "png", 640, 480);
+                    }
+                    ppt.Close();
+                    //app.Presentations[1].Close();
                 }
-                //app.Presentations[1].Close();
 
                 // http://stackoverflow.com/questions/981547/powerpoint-launched-via-c-sharp-does-not-quit
                 GC.Collect();
                 GC.WaitForPendingFinalizers();
-                ppt.Close();
+//                ppt.Close();
                 Marshal.ReleaseComObject(ppt);
                 app.Quit();
                 Marshal.ReleaseComObject(app);
@@ -121,6 +133,47 @@ namespace PPTBrowser
                 e.Effect = DragDropEffects.None;
             }
 
+        }
+
+        void DirSearch(string sDir)
+        {
+            string strext = "*.pptx";
+            //listBox1.Items.Add(sDir);
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir, strext))
+                {
+                    listBox1.Items.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    DirSearch(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DirSearch(label3.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Folder Selection Dialog
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                label3.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ExtractSlidesFromPPT(true);
         }
     }
 }
